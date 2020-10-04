ab6e21ee2da086dcc2e10113c7fb2ab97a91a865
Aaron <acelizondo11@gmail.com>
2020-10-04 00:02:10 -0500

    Game to run with random computer code and player guessing code

--- oop_projects/mastermind.rb
+++ oop_projects/mastermind.rb
@@ -12,6 +12,17 @@ module Colors
     COLORS[rand(6)]
   end
 
+  def list_colors
+    color_string = "\nThe available colors are "
+    COLORS.each_with_index do |color, index|
+      if index%2 == 0
+        color_string += "\n"
+      end
+      color_string += color + " "
+    end
+    puts color_string
+  end
+
 end
 
 class Game
@@ -22,9 +33,26 @@ class Game
 
   def initialize(role, code)
     @role = "computer"
-    @secret_code = generate_secret_code
+    #@secret_code = generate_secret_code
+    @secret_code = ['red', 'blue', 'yellow', 'purple']
   end
 
+  def start_game(player)
+    until player.guess_count > Game.max_guess || player.guess == @secret_code
+      player.ask_for_guess
+      guess_result = check_code(player.guess)
+      unless guess_result[0] == 4 && guess_result[1] ==4
+        display_guess_results(guess_result)
+      end
+    end
+    if(player.guess == @secret_code)
+      puts "You successfully guessed the code!"
+    else
+      puts "You ran out of turns, try again."
+    end
+  end
+
+  private
   def generate_secret_code
     code = Array.new(4)
     code.map! do |code| 
@@ -32,9 +60,33 @@ class Game
     end
   end
 
+  def check_code(player_code)
+    match_array = []
+    if player_code == self.secret_code
+      match_array = [4,4]
+    else
+      match_array = [0,0]
+      @secret_code.each_with_index do |code, index|
+        p player_code
+        if player_code[index] == code
+          match_array[0] += 1
+          match_array[1] += 1
+        elsif player_code.include?(code)
+          match_array[1] += 1
+        end
+      end
+    end
+    match_array
+  end
+
+  def display_guess_results(results)
+    puts "Your guess had #{results[0]} colors in the right position and #{results[1]} total correct colors"
+  end
+
   def self.max_guess
     @@max_guess
   end
+
 
 end
 
@@ -49,11 +101,43 @@ class Player
     @guess_count  = 0
   end
 
+  def ask_for_guess
+    valid_response = false
+    until valid_response
+      puts "Please enter your guess in the format 'color color color color'(to list available color options enter 'options'):"
+      response = gets.chomp
+      valid_response = validate_response(response)
+    end
+    @guess_count += 1
+    @guess = valid_response
+  end
+
+  private
+  def validate_response(guess_string)
+    guess_array = []
+    guess_string = guess_string.split(" ")
+    guess_string.each do |color_guess|
+      color_guess = color_guess.downcase
+      if is_valid?(color_guess)
+        guess_array.push(color_guess)
+      elsif color_guess == 'options'
+        list_colors
+        return false
+      else 
+         return false
+      end
+    end
+    guess_array
+  end
+
+  def is_valid?(color_string)
+    COLORS.include?(color_string)
+  end
+
 end
 
 new_game = Game.new("computer", 1)
 player = Player.new("player")
 
-until player.guess_count > Game.max_guess || player.guess == new_game.secret_code
-  
-end
+
+new_game.start_game(player)
