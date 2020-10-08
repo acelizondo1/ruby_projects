8e52fdac5eb7d41178526edf349075c719c26bf7
Aaron <acelizondo11@gmail.com>
2020-10-07 20:54:45 -0500

    Added save and load functions for game

--- file_serialization_projects/hangman.rb
+++ file_serialization_projects/hangman.rb
@@ -1,4 +1,4 @@
-
+require 'yaml'
 
 class Hangman
     @@INCORRECT_GUESSES_ALLOWED = 7
@@ -10,7 +10,7 @@ class Hangman
         ["  _____ ", " |     |", " O     |", "_|_    |", "       |", "       |", "_______|"],
         ["  _____ ", " |     |", " O     |", "_|_    |", " |     |", "       |", "_______|"],
         ["  _____ ", " |     |", " O     |", "_|_    |", " |     |", "/      |", "_______|"],
-        ["  _____ ", " |     |", " O     |", "_|_    |", " |     |", "/  \    |", "_______|"]
+        ["  _____ ", " |     |", " O     |", "_|_    |", " |     |", "/ \\    |", "_______|"]
     ]
     
     def initialize()
@@ -22,17 +22,23 @@ class Hangman
     end
 
     def start_game
+        puts "Would you like to load the last game?"
+        load_response = ""
+        until load_response == "yes" || load_response == "no"
+            load_response = gets.chomp.downcase
+        end
+        load_game if load_response == "yes"
         until @incorrect_guesses == @@INCORRECT_GUESSES_ALLOWED || @guessed_word == @word
+            p @word
             puts "You have #{@@INCORRECT_GUESSES_ALLOWED - @incorrect_guesses} wrong guesses left!\n"
             display_hangman
             display_word
             guessed_letter = ask_for_guess
-            check_guess(guessed_letter)
+            guessed_letter == "save" ? save_game : check_guess(guessed_letter)
         end
         if @guessed_word == @word
             puts "Congrats! You won the game!"
         else
-            #@incorrect_guesses += 1
             display_hangman
             puts "You ran out of guesses. Try Again!"
         end
@@ -53,8 +59,8 @@ class Hangman
 
     def ask_for_guess
         response_letter = ""
-        until response_letter.length == 1 && letter?(response_letter) && !guessed?(response_letter)
-            puts "Please enter your next guess(must be a letter(must be a letter not yet guessed):"
+        until (response_letter.length == 1 && letter?(response_letter) && !guessed?(response_letter)) || response_letter.downcase == "save"
+            puts "Please enter your next guess(must be a letter(must be a letter not yet guessed) or enter 'save' to save yor game for later:"
             response_letter = gets.chomp()
         end
         response_letter.downcase
@@ -82,6 +88,28 @@ class Hangman
         puts "\n\nIncorrectly guessed letters: \n"
         @incorrect_letters.each { |letter| print letter + ", " }
         puts "\n\n"
+    end
+
+    def save_game
+        File.open("save_game.yml", "w") do |file|
+            data = YAML.dump ({
+                :word => @word,
+                :guessed_word => @guessed_word,
+                :incorrect_letters => @incorrect_letters,
+                :incorrect_guesses => @incorrect_guesses
+            })
+            file.puts data
+        end
+        puts "You've successfully saved your game. Goodbye"
+        exit(0)
+    end
+
+    def load_game
+        load_data = YAML.load(File.read("save_game.yml"))
+        @word = load_data[:word]
+        @guessed_word = load_data[:guessed_word]
+        @incorrect_letters = load_data[:incorrect_letters]
+        @incorrect_guesses = load_data[:incorrect_guesses]
     end
 
     def display_hangman
